f44da0f371a83188b2c5df72ca9a7bc83f5214ed
Aaron Creed P. Celindro <aaroncreedpcelindro@gmail.com>
2026-05-25 01:25:57 +0800

    Create COUNT_03.sql

---
+++ [SQL]_12_COUNT/COUNT_03.sql
@@ -0,0 +1,14 @@
+-- Q1. Count all teachers in the teachers table.
+SELECT COUNT(*) AS num_teacher FROM teachers
+
+-- Q2. Count all classrooms in the classrooms table.
+SELECT COUNT(*) as classroom_count FROM classrooms
+
+-- Q3. Count all enrollment records in the enrollments table.
+SELECT COUNT(*) AS enrollment_count FROM enrollments
+
+-- Q4. Count all books in the library table.
+SELECT COUNT(*) AS num_books FROM library
+
+-- Q5. Count all events in the events table.
+SELECT COUNT(*) as events_count FROM events
