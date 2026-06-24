1f6b5f51814ae2293283579c28807aef31336410
Aaron Creed P. Celindro <aaroncreedpcelindro@gmail.com>
2026-06-24 16:10:51 +0800

    Create COUNT_06.sql

---
+++ [SQL]_12_COUNT/COUNT_06.sql
@@ -0,0 +1,14 @@
+-- Q1. Count the total number of doctors in the doctors table.
+SELECT COUNT(*) AS total_num_doctors FROM doctors
+
+-- Q2. Count the total number of patients in the patients table.
+SELECT COUNT(*) AS total_num_patients FROM patients
+
+-- Q3. Count the total number of records in the medicines table.
+SELECT COUNT(*) AS total_records FROM medicines
+
+-- Q4. Count the total number of procedures performed in the procedures table.
+SELECT COUNT(*) AS total_num_prodcedures FROM procedures
+
+-- Q5. Count the total number of staff members.
+SELECT COUNT(*) AS total_num_staffs FROM staff
